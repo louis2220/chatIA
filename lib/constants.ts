ec4ad17f570c04506ca8d31a48d1d85fe9a9443e
louis2220 <ph5652115@gmail.com>
2026-04-17 21:13:44 -0300

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -13,8 +13,8 @@ export const guestRegex = /^guest-\d+$/;
 export const DUMMY_PASSWORD = generateDummyPassword();
 
 export const suggestions = [
-  "What are the advantages of using Next.js?",
-  "Write code to demonstrate Dijkstra's algorithm",
-  "Help me write an essay about Silicon Valley",
-  "What is the weather in San Francisco?",
+  "Como estudar melhor para o ENEM?",
+  "Como criar um app simples usando só o celular",
+  "Me explica como funciona o mercado de criptomoedas?",
+  "Explica inteligência artificial de um jeito simples",
 ];
